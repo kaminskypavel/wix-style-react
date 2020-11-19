933e1eecd6366dc4930079d765733866f0a51c23
sivanharel <sivanh@wix.com>
2020-11-19 14:09:32 +0200

    Add st.css files in order to expose the classes for theming Change the files name for meaningful names

---
+++ src/Calendar/DatePickerHead/DatePickerHead.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import ChevronLeftLarge from 'wix-ui-icons-common/ChevronLeftLarge';
+import ChevronRightLarge from 'wix-ui-icons-common/ChevronRightLarge';
+
+import YearDropdown from './YearDropdown/YearDropdown';
+import MonthDropdown from './MonthDropdown/MonthDropdown';
+import { classes } from './DatePickerHead.st.css';
+import Text from '../../Text';
+
+const getMonthName = (months, month) => months[month] || months[0];
+
+const DatePickerHead = ({
+  className,
+  date,
+  localeUtils,
+  onChange,
+  onLeftArrowClick,
+  onRightArrowClick,
+  showMonthDropdown,
+  showYearDropdown,
+}) => {
+  return (
+    <div
+      data-hook="datepicker-head"
+      className={classnames(classes.root, className)}
+    >
+      <div
+        className={classnames(classes.arrow, classes.arrowLeft)}
+        data-hook="datepicker-left-arrow"
+        onClick={onLeftArrowClick}
+      >
+        <ChevronLeftLarge className={classes.arrowIcon} />
+      </div>
+
+      {showMonthDropdown ? (
+        <MonthDropdown
+          date={date}
+          onChange={onChange}
+          months={localeUtils.getMonths()}
+        />
+      ) : (
+        <Text
+          className={classes.caption}
+          weight="normal"
+          dataHook={'datepicker-month-caption'}
+        >
+          {getMonthName(localeUtils.getMonths(), date.getMonth())}
+        </Text>
+      )}
+
+      {showYearDropdown ? (
+        <YearDropdown date={date} onChange={onChange} />
+      ) : (
+        <Text
+          className={classes.caption}
+          weight="normal"
+          dataHook={'datepicker-year-caption'}
+        >
+          {date.getFullYear()}
+        </Text>
+      )}
+
+      <div
+        className={classnames(classes.arrow, classes.arrowRight)}
+        data-hook="datepicker-right-arrow"
+        onClick={onRightArrowClick}
+      >
+        <ChevronRightLarge className={classes.arrowIcon} />
+      </div>
+    </div>
+  );
+};
+
+DatePickerHead.propTypes = {
+  date: PropTypes.object.isRequired,
+  localeUtils: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onLeftArrowClick: PropTypes.func.isRequired,
+  onRightArrowClick: PropTypes.func.isRequired,
+  showMonthDropdown: PropTypes.bool,
+  showYearDropdown: PropTypes.bool,
+};
+
+export default DatePickerHead;
