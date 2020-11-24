6badb9fd949e7c349c9e1b6945674148933de1f9
yaron nachshon <yaronn@wix.com>
2020-11-24 18:28:17 +0200

    add visual test

--- src/AnalyticsSummaryCard/test/AnalyticsSummaryCard.visual.js
+++ src/AnalyticsSummaryCard/test/AnalyticsSummaryCard.visual.js
@@ -1,6 +1,16 @@
+/* eslint-disable no-console */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import AnalyticsSummaryCard from '../AnalyticsSummaryCard';
+import Refresh from 'wix-ui-icons-common/Refresh';
+
+import IconButton from '../../IconButton';
+
+const chartData = [
+  { label: new Date('Thu Sep 4 2020'), value: 3 },
+  { label: new Date('Thu Sep 5 2020'), value: 17 },
+  { label: new Date('Thu Sep 6 2020'), value: 18 },
+];
 
 const commonProps = {
   // use for repeated props across the tests (e.g. {buttonText: 'example'})
@@ -13,7 +23,25 @@ const tests = [
       {
         it: 'default', // prop variation (e.g. small)
         props: {
-          // the simulation (e.g. {size: "small"})
+          title: 'Sessions',
+          titleTooltip: 'tooltip',
+          value: '1,9K',
+          valueTooltip: '1,943',
+          percentage: 12,
+          invertedPercentage: false,
+          isLoading: false,
+          ctaButton: (
+            <IconButton size="tiny">
+              <Refresh />
+            </IconButton>
+          ),
+          onCTAClick: () => console.log('refresh click'),
+          onClick: () => console.log('general click'),
+          onChartHover: () => console.log('on chart hover'),
+          chartWidth: 169,
+          chartData,
+          chartColorHex: '#3899ec',
+          footer: <div> This is footer</div>,
         },
       },
     ],
